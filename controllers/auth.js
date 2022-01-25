e613873f63024ba5af046e8ad5086e9c14b788b1
madnessTroy <huutoanchu@gmail.com>
2022-01-25 10:09:50 +0700

    LAB_8.4

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,13 @@
 exports.getLogin = (req, res) => {
-	const isLoggedIn = req.get('Cookie').split('=')[1].trim();
+	// const isLoggedIn = req.get('Cookie').split('=')[1].trim() === 'true';
 	res.render('auth/login', {
 		pageTitle: 'Log In',
 		path: '/login',
-		isAuthenticated: isLoggedIn,
+		isAuthenticated: false,
 	});
 };
 
 exports.postLogin = (req, res) => {
-	res.setHeader('Set-Cookie', 'loggedIn=true');
+	res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
 	res.redirect('/');
 };
