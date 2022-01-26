49b7d8a3994597a6f7198e87282f76caa6bc55d4
madnessTroy <huutoanchu@gmail.com>
2022-01-26 08:49:16 +0700

    LAB_8.16

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,16 +39,16 @@ exports.postSignup = (req, res, next) => {
 			if (userDoc) {
 				return res.redirect('/signup');
 			}
-			return bcrypt.hash(password, 12);
-		})
-		.then((hashedPassword) => {
-			const user = new User({
-				email,
-				password: hashedPassword,
-				cart: { items: [] },
+			return bcrypt.hash(password, 12).then((hashedPassword) => {
+				const user = new User({
+					email,
+					password: hashedPassword,
+					cart: { items: [] },
+				});
+				return user.save();
 			});
-			return user.save();
 		})
+
 		.then(() => {
 			res.redirect('/login');
 		})
