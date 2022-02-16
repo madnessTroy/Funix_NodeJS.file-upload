fa2a2332d8f5fc882161269870700350732955ee
madnessTroy <huutoanchu@gmail.com>
2022-02-16 19:52:38 +0700

    LAB_9.16

--- app.js
+++ app.js
@@ -73,7 +73,9 @@ app.use(shopRoutes);
 app.use(authRoutes);
 app.get('/500', errorController.get500);
 app.use(errorController.get404);
-
+app.use((error, req, res, next) => {
+	res.redirect('/500');
+});
 // Mongoose connect
 mongoose
 	.connect(MONGODB_URI)
